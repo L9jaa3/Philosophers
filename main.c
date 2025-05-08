36d78d29ee89ccc08641b6a94b7a315b501a0cfd
elouarrak ismail <itsmeelouarr@gmail.com>
2025-05-08 16:08:27 +0100

    updating ./philo

--- main.c
+++ main.c
@@ -6,53 +6,70 @@
 /*   By: ielouarr <ielouarr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 10:31:45 by ielouarr          #+#    #+#             */
-/*   Updated: 2025/05/01 13:33:52 by ielouarr         ###   ########.fr       */
+/*   Updated: 2025/05/08 15:58:18 by ielouarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int    ft_store_args(int ac, char **av, t_global *args)
+t_global *ft_store_args(int ac, char **av)
 {
+    t_global *args;
+    
+    args = ft_malloc(sizeof(t_global));
+    if (!args)
+        return (NULL);
+    memset(args, 0, sizeof(t_global));
     args->philosophers_nb = ft_atol(av[1]);
     args->time_to_die = ft_atol(av[2]);
     args->time_to_eat = ft_atol(av[3]);
     args->time_to_sleep = ft_atol(av[4]);
-    
     if (args->time_to_die < 60 || args->time_to_eat < 60 || args->time_to_sleep < 60)
-        return (ft_error("Use timestamps greater than 60ms for realistic simulation"));
-    
+    {
+        ft_error("Use timestamps greater than 60ms for realistic simulation");
+        free(args);
+        return (NULL);
+    }
     if (ac == 6)
         args->must_eat = ft_atol(av[5]);
     else
-        args->must_eat = -1;
-        
-    if (args->philosophers_nb <= 0 || args->time_to_die <= 0 || 
-        args->time_to_eat <= 0 || args->time_to_sleep <= 0 || 
+        args->must_eat = -1; 
+    if (args->philosophers_nb <= 0 || args->time_to_die <= 0 ||
+        args->time_to_eat <= 0 || args->time_to_sleep <= 0 ||
         (ac == 6 && args->must_eat <= 0))
-        return (ft_error("Error: Invalid argument values.\n"));
-    
-    return (0);
+    {
+        ft_error("Error: Invalid argument values.");
+        free(args);
+        return (NULL);
+    }
+    return (args);
 }
 
 int main(int ac, char **av)
 {
-    t_global    *args;
+    t_global *args;
 
     if (ac < 5 || ac > 6)
-        return (ft_error("Error: incorrect number of arguments")); 
+    {
+        ft_error("Error: incorrect number of arguments");
+        return (1);
+    }
+    
     if (ft_atol(av[1]) < 1 || ft_atol(av[1]) > 200)
-        return (ft_error("philosophers arg is too low or more than required [1 - 200]"));  
-    args = ft_malloc(sizeof(t_global));
+    {
+        ft_error("philosophers arg is too low or more than required [1 - 200]");
+        return (1);
+    }
+    
+    args = ft_store_args(ac, av);
     if (!args)
         return (1);
-    memset(args, 0, sizeof(t_global));
-    if (ft_store_args(ac, av, args) != 0)
-        return (1); 
+        
     mutex_init(args);
     philo_spawner(args);
     monitoring(args);
     destroy_mutex(args);
     free(args);
+    
     return (0);
 }
